84c59799f57d99c7ff148866adba68ee023b8bd9
zdag27 <50047402+zdag27@users.noreply.github.com>
2020-01-05 07:22:42 +0100

    Add files via upload

--- easy_dial.cpp
+++ easy_dial.cpp
@@ -51,9 +51,10 @@ void easy_dial::buscahavueltomalditoesinmatable(node* it,vector<string>& s,bool 
 };
 
 easy_dial::easy_dial(const call_registry& R) throw(error){
+    nivel=0;
     vector <phone> v;
     R.dump(v);
-
+    _it=NULL;
     // cout << "Esto es lo del dump: " << endl;
     if (v.size() > 0) {
         // cout << "insertando " << v[0].nom() << endl;
@@ -65,99 +66,50 @@ easy_dial::easy_dial(const call_registry& R) throw(error){
         _raiz->cen = NULL;
         _it=_raiz;
         for (int i=v.size()-2; i>=0; --i){
-            // cout << "insertando " << v[i].nom() << endl;
-            inserta(v[i]);
+            node *it=_raiz;
+            cout<<v[i].nom();
+            inserta(v[i],it,0);
         }
     } else {
         _raiz = NULL;
     }
 };
 
-void easy_dial::inserta(phone p){
-    node* it = _raiz;
-    // esquerra = ultim node de l'esquerra visitat
-    node* esquerra = NULL;
-    // dreta = ultim node de la dreta que hem visitat
-    node* dreta = NULL;
-    node* amunt = NULL;
-
-    // aconseguim el nom del phone que estem insertant i afegim un caracter "#" al final
-    string nom = p.nom() + "#";
-    int nivell = 0;
-    bool trobat = false;
-    // Invariant: trobat es fals mentre no arribem a on s'ha de crear el nou node
-    while(!trobat){
-        // if = hem trobat a on va el node, finalitzem
-        // else = queden més desplaçaments, actualitzem it_anterior
-        if (it == NULL){
-            trobat = true;
-        } else {
-            // Moure it
-            char lletra_phone = nom[nivell];
-            char lletra_arbre;
-            // Cas: node conté una paraula a
-            if (it->cell.nom().size() == nivell){
-                lletra_arbre = '#';
-            } else {
-                lletra_arbre = it->cell.nom()[nivell];
-            }
-            //cout << "comparando " << nom << " con " << it->cell.nom() << endl;
-            if (lletra_phone == lletra_arbre){
-                //cout << nom << "bajó por " << it->cell.nom() << endl;
-                ++nivell;
-                amunt = it;
-                esquerra = NULL;
-                dreta = NULL;
-                it = it->cen;
-            } else if (lletra_phone < lletra_arbre){
-                //cout << nom << "se fue a la izquierda de " << it->cell.nom() << endl;
-                esquerra = it;
-                it = it->izq;
-            } else {
-                //cout << nom << "se fue a la derecha de " << it->cell.nom() << endl;
-                dreta = it;
-                it = it->der;
-            }
+void easy_dial::inserta(phone p,node* &it,int x){
+    if(it->cell.nom()[x]==p.nom()[x]){
+        if(it->cen==NULL){
+            it->cen=new node;
+            it->cen->cell=p;
+            it->cen->cen=NULL;
+            it->cen->izq=NULL;
+            it->cen->der=NULL;
+            it->cen->arr=it;
+        }else{
+            inserta(p,it->cen,x+1);
         }
-    } // fi while
-    /*
-    cout << "Entrando en dump" << endl;
-    dump(_raiz);
-    cout << "Salí de dump*" << endl;
-    */
-    it = new node;
-    it->cell = p;
-
-    if (esquerra != NULL)
-        esquerra->izq = it;
-    it->izq = NULL;
-
-    if (dreta != NULL)
-        dreta->der = it;
-    it->der = NULL;
-
-    if (amunt != NULL){
-        it->arr = amunt;
-        if (amunt->cen == NULL){
-            amunt->cen = it;
+    }else if(it->cell.nom()[x]>p.nom()[x]){
+        if(it->izq==NULL){
+            it->izq=new node;
+            it->izq->cell=p;
+            it->izq->cen=NULL;
+            it->izq->izq=NULL;
+            it->izq->der=NULL;
+            it->izq->arr=it->arr;
+        }else{
+            inserta(p,it->izq,x);
         }
-    } else {
-        it->arr = NULL;
+    }else{
+        if(it->der==NULL){
+            it->der=new node;
+            it->der->cell=p;
+            it->der->cen=NULL;
+            it->der->izq=NULL;
+            it->der->der=NULL;
+            it->der->arr=it->arr;
+        }else{
+            inserta(p,it->der,x);
+        }
     }
-    it->cen = NULL;
-
-    /*
-    if (dreta != NULL){
-        cout << "DRETA = " << dreta->izq << ", " << dreta->der << ", " << dreta->arr << ", " << dreta->cen << endl;
-    } else if (esquerra != NULL) {
-        cout << "ESQUERRA = " << esquerra->izq << ", " << esquerra->der << ", " << esquerra->arr << ", " << esquerra->cen << endl;
-    }
-    cout << "IT = " << it->izq << ", " << it->der << ", " << it->arr << ", " << it->cen << endl;
-    cout << "Entrando en dump" << endl;
-    dump(_raiz);
-    cout << "Salí de dump*" << endl;
-    */
-
 };
 
 void easy_dial::dump(node* it) const{
@@ -232,62 +184,38 @@ void easy_dial::borrar(node *it){
 }
 
 string easy_dial::inici() throw(){
-    this->nivel = 0;
-    if(!init){
-        this->init = !this->init;
+    if(_raiz==NULL){
+        return "";
+    }else{
+        init=true;
+        _it=_raiz->cen;
+        return _raiz->cell.nom();
     }
-    /*
-    if(init)
-        cout << "Eureka, era cierto" << endl;
-    else
-        cout << "Rayos y retruecanos" << endl;
-    */
-    _it = _raiz;
-    string nom = "";
-    if (_it != NULL) {
-        nom = _raiz->cell.nom();
-    }
-    return nom;
 };
 
 string easy_dial::seguent(char c) throw(error){
-    if (_it != NULL){
-        node* aux = _it->cen;
-        if(aux != NULL){
-            ++nivel;
-            bool trobat = false;
-            while(!trobat){
-                if (aux != NULL){
-                    throw (error(ErrPrefixIndef));
-                } else {
-                    if (c ==aux->cell.nom()[nivel]){
-                        _it = aux;
-                        trobat = true;
-                    } else if (c < aux->cell.nom()[nivel]) {
-                        aux = aux->izq;
-                    } else {
-                        aux = aux->der;
-                    }
-                }
-            }
-        } else {
-            throw (error(ErrPrefixIndef));
+    cout<<"eooiiiiio";
+    if(_it!=NULL){
+        cout<<"eooooooo";
+        node* it=_it;
+        while(it->cell.nom()[nivel]!=c){
+            if(it->cell.nom()[nivel]>c)
+                it=it->izq;
+            else
+                it=it->der;
         }
-    } else {
+        if(it==NULL and !init)
+            throw (error(ErrPrefixIndef));
+        else if(it==NULL){
+            init=false;
+            return "";
+        }else{
+            _it=it->cen;
+            return it->cell.nom();
+        }
+    }else{
         throw (error(ErrPrefixIndef));
     }
-    return(_it->cell.nom());
-
-    /*
-    arb *auxi=_it->cen;
-    busc(auxi,c);
-    if(auxi==NULL){
-            throw (error(ErrPrefixIndef));
-    }else{
-            _it=auxi;
-            return(_it->cell.nom());
-    }
-    */
 };
 
 string easy_dial::anterior() throw(error){
